ad7b8d37d3919ee2113c0febd7c918e4d9c27aaf
Abel <abel.romerotavio@gmail.com>
2024-12-26 21:04:38 +0000

    Created two new classes which model the format in which I will be adapting the data from the ramdomUser page.

---
+++ src/main/java/control/pojo/RamdomUser.java
@@ -0,0 +1,28 @@
+package control.pojo;
+
+public class RamdomUser {
+    private String gender;
+    private Name name;
+    private String email;
+    private Picture picture;
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Picture getPicture() {
+        return picture;
+    }
+
+    public record Name(String title, String first, String Last) {}
+
+    public record Picture(String large, String medium, String thumbnail) {}
+}
---
+++ src/main/java/control/pojo/RamdomUserResponse.java
@@ -0,0 +1,11 @@
+package control.pojo;
+
+import java.util.List;
+
+public class RamdomUserResponse {
+    private List<RamdomUser> results;
+
+    public List<RamdomUser> getResults() {
+        return results;
+    }
+}
